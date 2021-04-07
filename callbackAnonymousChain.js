32c6bf54dceddfa88b618dd74705e4fb03024601
sadjadtavakoli <sadjadtavakoli@gmail.com>
2021-04-06 19:53:23 -0700

    add new function

--- callbackAnonymousChain.js
+++ callbackAnonymousChain.js
@@ -2,6 +2,10 @@ function caller(callback, callback2, callback3){
     callback(callback2(callback3))
 }
 
+function caller2(callback, callback2, callback3){
+    callback(callback2(callback3))
+}
+
 caller((callerInput)=>{
     // do nothing
 }, (callback)=>{
