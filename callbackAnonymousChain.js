5e52f08200a50947e8ffe346b5f32e74516a29f0
sadjadtavakoli <sadjadtavakoli@gmail.com>
2021-04-06 19:55:36 -0700

    updatefunction

--- callbackAnonymousChain.js
+++ callbackAnonymousChain.js
@@ -1,9 +1,10 @@
-function caller(callback, callback2, callback3){
+function caller_JADID(callback, callback2, callback3){
     callback(callback2(callback3))
 }
 
 function caller2(callback, callback2, callback3){
     callback(callback2(callback3))
+    return "slm"
 }
 
 caller((callerInput)=>{
