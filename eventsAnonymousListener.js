402ecae35b19af0adff16ecc7ec9b49a5b4c039b
sadjad-tavakoli <sadjadtavakoli@gmail.com>
2021-04-07 18:30:13 -0700

    Update eventsAnonymousListener.js

--- eventsAnonymousListener.js
+++ eventsAnonymousListener.js
@@ -4,6 +4,8 @@ var eventEmitter = new events.EventEmitter();
 
 eventEmitter.addListener('connection', ()=>{
    //  do nithign
+   // some new lines 
+   // let's see what's going on here
 });
 
 eventEmitter.emit('connection');
