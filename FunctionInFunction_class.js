880b05ce05fc028bce63d035a3596eca6b266171
sadjad-tavakoli <sadjadtavakoli@gmail.com>
2021-04-07 18:51:23 -0700

    Create FunctionInFunction_class.js

---
+++ FunctionInFunction_class.js
@@ -0,0 +1,31 @@
+class SimpleClass{
+    constructor(variable_1, variable_2) {
+        this.variable_1 = variable_1;
+        this.variable_2 = variable_2;
+    }
+
+    classFunction(){
+        function innerFoo(){
+         // do nothing
+        }
+        return this.variable_1
+    }
+    variable_3 = 14;
+
+    static classStaticFunction(){
+        return 100;
+    }
+}
+
+
+let simpleInstance = new SimpleClass(4, 5)
+
+let tempt_1 = simpleInstance.variable_1
+let tempt_2 = simpleInstance.variable_2
+
+simpleInstance.variable_3 = tempt_2 * tempt_1;
+
+let functionCallResult = simpleInstance.classFunction()
+
+
+let classStaticMethodCall = SimpleClass.classStaticFunction();
